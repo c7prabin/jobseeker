03694d2ea476024272d8fcdcb5b507edaf4baf45
praveen magar <praveenmagar77@gmail.com>
2017-02-25 08:14:43 +0545

    this is my jobseeker post

---
+++ about.php
@@ -0,0 +1,129 @@
+<html lang="en">
+    <head>
+        <title>Bootstrap Case</title>
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="css/job.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="col-sm-4">
+                <nav class="navbar ">
+                    <div class="container">
+                        <div class="navbar-header">
+                            <h1> JobSeeKer</h1>
+                        </div>
+                        <div class="col-sm-4">
+                        </div>
+                        <div class="col-sm-4">
+                            <p>   <a href="#" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-search"></span> Search</a></
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <div class="container">
+            <nav class="navbar navbar-default" style="background-color: white" >
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>                        
+                        </button>
+                    </div>
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                        <ul class="nav navbar-nav">
+                            <li class="home"><a href="job.php">Home</a></li>
+                            <li class="profile" class="dropdown">
+                                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Profile <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Home</a></li>
+                                    <li><a href="#">Page 1-2</a></li>
+                                    <li><a href="#">Page 1-3</a></li>
+                                </ul>
+                            </li>
+                            <li class="about"><a href="#">About</a></li>
+                            <li class="contact"><a href="#">Contact</a></li>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="login"><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                            <li class="sign"><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div class="container">
+            <div class="allow" >
+     
+                    <div class="details">
+                        <b class="titletop">WELCOME TO JOBSEEKER</b>
+                         <b class="titlebottom" style="color:brown">GOOD</b>
+                        <b class="titlebottom" style="color:green">OPPURTINITY</b>
+                        <div class="col-sm-12">
+                        <div class="text">
+                           This is one of the  best site in the world for the job searching
+                           we promise you to provide the best job according to your level
+                           which suit you. We also can provide training if required for the
+                           job. We can develop best skills required for you to compute any competitors in this
+                           world.
+                            <a href="#"> readmore......</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+        <div class="col-sm-12">
+                 <div class="hrline"></div>
+        </div>
+            </div>
+            <div class="team">
+            <div class="col-sm-4"></div>
+            <div class="col-sm-6">
+                <b class="titletop"><u>TEAM MEMBERS</u></b>
+            </div>
+            
+            <div class="col-sm-2"></div>
+            </div>
+            <div class="panel panel-danger">
+                <div class="panel-body">
+                    
+                <div class="col-sm-2"></div>
+                <div class="col-sm-2"> <img src="img/5.jpg" class="img-circle" class="img-responsive" width="100" height="100">
+                    <h3>Praveen Magar</h3>
+				<small>Team Leader</small>
+                </div>
+            <div class="col-sm-2">
+                 <img src="img/7.jpg" class="img-circle" class="img-responsive" width="100" height="100">
+                 <h3>cristano ronaldo</h3>
+				<small>UI designer</small>
+            </div>
+            <div class="col-sm-2"> <img src="img/6.jpg" class="img-circle" class="img-responsive" width="100" height="100">
+            <h3>Annie Lady</h3>
+				<small>Web developer</small>
+            </div>
+            <div class="col-sm-2"> <img src="img/8.jpg" class="img-circle" class="img-responsive" width="100" height="100">
+            <h3>Jane lucy</h3>
+				<small>PHP developer</small>
+            </div>
+           <div class="col-sm-2"></div>
+            </div>
+                </div>
+        </div>
+         <footer>
+            (&copy; 2016 -EduCash)
+            All Rights Reserved
+            <div class="pull-right">
+                Find Us on: 
+                <ul>
+                    <li><a href="#"><i class="fa fa-facebook-official fa-lg"></i></a></li>
+                    <li><a href="#"><i class="fa fa-google-plus fa-lg"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter fa-lg"></i></a></li>
+                    <li><a href="#"><i class="fa fa-pinterest fa-lg"></i></a></li>
+                </ul>
+            </div>
+        </footer>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <title>Bootstrap Case</title>
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/prabin.css">
+</head>
+<body>
+<div class="container">
+<nav class="navbar navbar-inverse">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Profile <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Page 1-2</a></li>
+            <li><a href="#">Page 1-3</a></li>
+          </ul>
+        </li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</div> 
+<div class="container">
+  <div class="jumbotron">
+    <h1> JOB PROVIDER </h1>      
+    <p>Every unemployed youth please contact us for the job. We promise you to provide your level job.</p>      
+    <a href="#" class="btn btn-danger btn-lg"><span class="glyphicon glyphicon-search"></span> Search</a>
+  </div>
+      <div class="col-sm-12">
+	<div class="panel panel-primary">
+ <div class="panel-heading">
+Latest news
+ </div>
+      <div class="panel-body">
+<p><img src="img/1.jpg" class="img-responsive img-thumbnail" width="100px" align="left" hspace="3" vspace="3">
+    the latest news about the job is posted here in this page.
+				</p>
+</div><div class="panel-body">
+<p style="color: black"><img src="img/2.jpg" class="img-responsive img-thumbnail" width="100px" align="left" hspace="3" vspace="3">
+    This is my page and i promise to provide job to all my friends who are in the search of the best job according to their label.</p>
+</div>
+        </div>
+      </div>
+    <div class="col-sm-8">
+        <div class="panel panel-danger"> 
+            <div class="panel-heading">
+                updated news
+            </div>
+            <div class="panel-body" style="background-color:#999966">
+                <p style="color:blue "  >if you want to know the detail about all those things then please contact us.
+                for all the contact details you can go to our website through the login  .
+                it includes all the detail of the following information.
+                In the process of the job searching we need to understand the basic important of the job. 
+                we must understand our level and txt the following.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-4"><img src="img/1.jpg" class="img-responsive "  ></div>
+    <div class="clearfix visible-lg"></div>
+</div>
+    <div class="container-fluid">
+    <footer>
+(&copy; 2016 -EduCash)
+				All Rights Reserved
+				<div class="pull-right">
+				Find Us on: 
+					<ul>
+						<li><a href="#"><i class="fa fa-facebook-official fa-lg"></i></a></li>
+						<li><a href="#"><i class="fa fa-google-plus fa-lg"></i></a></li>
+						<li><a href="#"><i class="fa fa-twitter fa-lg"></i></a></li>
+						<li><a href="#"><i class="fa fa-pinterest fa-lg"></i></a></li>
+					</ul>
+				</div>
+</footer>
+    </div>
+</body>
+</html>
